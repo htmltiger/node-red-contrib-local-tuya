d218aa5e9b585117531ebaa8fff04bc1ee7bd6cc
htmltiger <1429451+htmltiger@users.noreply.github.com>
2023-06-28 20:58:17 +0100

    Update local-tuya.js

--- local-tuya.js
+++ local-tuya.js
@@ -11,7 +11,7 @@ module.exports = function(RED) {
 		this.Key = config.devKey;
 		this.Ip = config.devIp;
 		this.version = config.protocolVer;
-		let dev_info;
+		let dev_info = {name: config.name, ip: config.Ip, id: config.Id};
 		let tuyaDevice;
 		let tuyaload = true;
 		node.status({fill:"grey",shape:"dot",text: "Standby"});
@@ -20,10 +20,7 @@ module.exports = function(RED) {
 		let retrysec=1;
 		let retrytimer=null;
 		let connTime=Date.now();
-		let day = 0;
-		let hrs = 0;
-		let mins = 0;
-		let sec = 0;
+		let lastError = "";
 		let v=0;
 		let retryReset=function(){retrysec=5;};
 		let connectToDevice=function() {
@@ -38,19 +35,24 @@ module.exports = function(RED) {
 				everconnected=true;
 				tuyaDevice.connect().then( () => {
 				}, (reason) => {
+					try{clearTimeout(autoRetry);} catch(e) {}
+					autoRetry = setTimeout(connectToDevice, (retrysec*1000));
 					node.send([null,{status: null, payload: reason, data: dev_info}]);
 					reason=JSON.stringify(reason);
 					node.status({fill:"red",shape:"ring",text:"Failed: " + reason});
-					if(reason.indexOf("ENETUNREACH")<0){
-						node.warn("dc: " + dev_info.name + " " + reason);
-					}else{node.warn(reason);}
+					if(reason != "{}" && reason != lastError){
+						lastError = reason;
+						if(reason.indexOf("ENETUNREACH")<0){
+							node.warn("dc: " + dev_info.name + " " + reason);
+						}else{node.warn(reason);}
+					}
 				});
 			});
 		};
 
 		let disconnectDevice=function(deleted){
 			autoReconnect = deleted ? false : true;
-			try	{tuyaDevice.disconnect();} catch(e) {}
+			try{tuyaDevice.disconnect();} catch(e) {}
 		};
 
 
@@ -74,7 +76,7 @@ module.exports = function(RED) {
 					return;
 				}
 				tuyaload = false;
-				dev_info =  {name:this.name,ip:this.Ip,id:this.Id};
+				dev_info.ip=this.Ip;
 				tuyaDevice = new TuyaDev({ 
 					id: this.Id, 
 					key: this.Key, 
@@ -87,56 +89,60 @@ module.exports = function(RED) {
 				});
 					
 				tuyaDevice.on('disconnected', () => {
-					try	{
-						clearTimeout(autoRetry);
-					} catch(e) {}
+					v=Date.now() - connTime;
+					let day = v / 8.64e7 | 0;
+					let hrs = (v % 8.64e7) / 3.6e6 | 0;
+					let mins = Math.round((v % 3.6e6) / 6e4);
+					let sec = Math.floor((v / 1000) % 60);
+					
+					day = (day>0) ? ( day + ' Days ' ) : '';
+					day += hrs.toString().padStart(2, '0') + ':' + mins.toString().padStart(2, '0') + ':' + sec.toString().padStart(2, '0');
+					
+					try{clearTimeout(autoRetry);} catch(e) {}
 					if (autoReconnect) {
 						retrysec++;
 						retrysec = Math.min(15, retrysec);
 						autoRetry = setTimeout(connectToDevice, (retrysec*1000));
+						
+						node.log("DC: " + this.name + " after "+day+", retry in " + retrysec + "s");
+					}else{
+						node.log("DC: " + this.name + " after "+day+", retry disabled");
+						
 					}
-					v=Date.now() - connTime;
-					day = v / 8.64e7 | 0;
-					hrs = (v % 8.64e7) / 3.6e6 | 0;
-					mins = Math.round((v % 3.6e6) / 6e4);
-					sec = Math.floor((v / 1000) % 60),
-					day = (day>0) ? ( day + ' Days ' ) : '';
-					day += hrs.toString().padStart(2, '0') + ':' + mins.toString().padStart(2, '0') + ':' + sec.toString().padStart(2, '0');
 					
-					node.log("DC: " + this.name + " after "+day+", retry in " + retrysec + "s");
 					v=new Date().toLocaleString();
 					node.status({fill:"red",shape:"ring",text:"Disconnected @ " + v});
 					node.send([null,{status: "offline", payload: "Disconnected", data: dev_info}]);
-					//msg = {data:dev_info}
-					//node.send(msg);
 				});
 
 				tuyaDevice.on('connected', () => {
 					node.log("Connected: " + this.name + " " + retrysec + "s" );
 					v=new Date().toLocaleString();
 					node.status({fill:"green",shape:"dot",text: this.Ip + " @ " + v});
-					try	{clearTimeout(autoRetry);} catch(e) {}
-					try	{clearTimeout(retrytimer);} catch(e) {}
+					try{clearTimeout(autoRetry);} catch(e) {}
+					try{clearTimeout(retrytimer);} catch(e) {}
 					retrytimer=setTimeout(retryReset, 120000);
+					autoReconnect = true;
 					connTime=Date.now();
-					node.send([null,{status: "online", payload: "Connected", data: dev_info}]);
+					node.send([null, {status: "online", payload: "Connected", data: dev_info}]);
 				});
 
 				tuyaDevice.on('error', error => {
 					let jerror=JSON.stringify(error);
-					if(jerror && jerror != "{}"){
+					if(jerror && jerror != "{}" && jerror != lastError){
+						lastError = jerror;
 						node.warn(jerror);
-						node.send([null,{status: null, payload: error, data:dev_info}]);
+						node.send([null, {status: null, payload: error, data:dev_info}]);
 					}
 					node.status({fill:"green",shape:"ring",text:"error: " + jerror});
 				});
 
 				tuyaDevice.on('data', (data, CommandType) => {
-					node.send([{data: dev_info, payload: data, CommandType: CommandType},null]); 
+					node.send([{data: dev_info, payload: data, CommandType: CommandType}, null]); 
 				});
 
 				tuyaDevice.on('dp-refresh', (data) => {
-					node.send([{data: dev_info, payload: data, CommandType: 18},null]);
+					node.send([{data: dev_info, payload: data, CommandType: 18}, null]);
 				});
 
 				connectToDevice();
@@ -170,9 +176,7 @@ module.exports = function(RED) {
 
 		node.on('close', function(removed, done) {
 			autoReconnect = false; tuyaload = false;
-			try	{
-				clearTimeout(autoRetry);
-			} catch(e) {}
+			try{clearTimeout(autoRetry);} catch(e) {}
 			if(everconnected){disconnectDevice(true); everconnected=false;}
 			done();
 		});
